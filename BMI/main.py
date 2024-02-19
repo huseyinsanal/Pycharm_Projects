c646737024143c3295e966f0e5ae3cdb7741dc5c
Hüseyin Şanal <huseyinnsanal@gmail.com>
2024-02-19 17:35:05 +0300

    bmi app added

--- BMI/main.py
+++ BMI/main.py
@@ -7,33 +7,6 @@ FONT = ('San Fransisco',13,"italic")
 
 
 
-def button_clicked():
-    global mass_float
-    global height_float
-    print(mass_float / (height_float/100)(height_float/100))
-    print("Button Clicked")
-
-'''
-
-user_height = input(int())
-user_mass = input(int())
-
-def button_clicked():
-    global user_mass
-    global user_height
-    print("Button Clicked")
-    print(user_mass, user_height)
-
-
-def calculation():
-    global user_mass
-    global user_height
-    result = [(user_height/100) * (user_height/100)] / user_mass
-    print(result)
-    
-'''
-
-
 window = tkinter.Tk()
 window.title("Body Mass Index Calculator")
 window.wm_minsize(x,y)
@@ -42,21 +15,33 @@ mass_label = tkinter.Label(text="Enter Your Weight(kg)",font=FONT)
 mass_label.pack()
 
 mass_entry = tkinter.Entry()
-mass_float = int(mass_entry)
 mass_entry.pack()
-mass_entry.focus()
 
 height_label = tkinter.Label(text="Enter Your Height(cm)",font=FONT)
 height_label.pack()
 
 height_entry = tkinter.Entry()
-height_float = int(height_entry)
 height_entry.pack()
+
+
+
+def button_clicked():
+
+    height_string = height_entry.get()
+    mass_string = mass_entry.get()
+
+    mass_value = int(mass_string)
+    height_value = int(height_string)
+
+    result = mass_value / ((height_value / 100)**2)
+    bmi_label.config(text=result)
+
+    print("Button Clicked")
 
 button = tkinter.Button(text="Calculate",command=button_clicked)
 button.pack()
 
-
-
+bmi_label = tkinter.Label(text="To see the result enter your informations...",font=FONT)
+bmi_label.pack()
 
 window.mainloop()
